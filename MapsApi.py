a1b9c73c43e185e843f6e0906f5cb34d346ca505
Moebamba17 <moebatish@gmail.com>
2019-07-18 00:33:02 -0400

    Added 2 functions to format user input and open the google maps API

--- MapsApi.py
+++ MapsApi.py
@@ -5,4 +5,10 @@ class MapsApi:
     def __init__(self,origin,destination):
         self.origin = origin
         self.destination = destination
-    
+    def format_input(self):
+        self.origin.strip()
+        self.destination.strip()
+        self.origin.replace(' ','+')
+        self.destination.replace(' ','+')
+    def open_url(self):
+        requests.get('https://www.google.com/maps/dir/?api=1&origin={}&destination={}&travelmode=driving'.format(self.origin,self.destination))
